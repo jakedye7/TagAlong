92522702fb76f55864f959fcfee1c0ad6dacb3cd
jakedye7 <jacob-dye@uiowa.edu>
2015-05-12 11:37:25 -0500

    initial commit

---
+++ TagAlongTests/TagAlongTests.h
@@ -0,0 +1,13 @@
+//
+//  TagAlongTests.h
+//  TagAlongTests
+//
+//  Created by iOS Dev on 11/17/14.
+//  Copyright (c) 2014 iOS Dev. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TagAlongTests : SenTestCase
+
+@end
---
+++ TagAlongTests/TagAlongTests.m
@@ -0,0 +1,32 @@
+//
+//  TagAlongTests.m
+//  TagAlongTests
+//
+//  Created by iOS Dev on 11/17/14.
+//  Copyright (c) 2014 iOS Dev. All rights reserved.
+//
+
+#import "TagAlongTests.h"
+
+@implementation TagAlongTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TagAlongTests");
+}
+
+@end
